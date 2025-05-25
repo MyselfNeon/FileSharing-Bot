185f2ae6c739868b32c8af194b35074007b99a28
NeonAnurag <justasneon@gmail.com>
2025-05-25 22:28:47 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("🎊 HELLO FROM NEON 🎊")
+    return web.json_response("|| NeonAnurag ||")
 
 
 
